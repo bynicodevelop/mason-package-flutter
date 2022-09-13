c8fc05718db42ba4bc50a43418019befe6e9e637
nico-develop <contact@nico-develop.fr>
2022-09-13 20:41:04 +0200

    #1 - Permet l'intégration de la licence et du changelogs

--- plugin/hooks/pre_gen.dart
+++ plugin/hooks/pre_gen.dart
@@ -28,6 +28,8 @@ void run(HookContext context) {
       '${context.vars["plugin_package_name"]}/example/lib',
       '${context.vars["plugin_package_name"]}/lib',
       '${context.vars["plugin_package_name"]}/test',
+      '${context.vars["plugin_package_name"]}/CHANGELOG.md',
+      '${context.vars["plugin_package_name"]}/LICENSE',
     ],
   );
 }
